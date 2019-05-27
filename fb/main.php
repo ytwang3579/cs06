1cb582fda450cc04654c0f79715e217eaf4fd8a0
106062328 <s106062328@SoftwareStudio.cs.nthu.edu.tw>
2019-05-27 21:25:09 +0800

    asdfff

--- fb/main.php
+++ fb/main.php
@@ -23,4 +23,20 @@ $user = $response->getGraphUser();
 
 var_dump($user);
 echo '<br><br>Hello ' . $user['name'];
-//echo '<br>Hello ' . $user['picture'];
+
+require_once("../config.php")
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO users (id, name)
+    VALUES ('".$user['id']."', '".$user['name']."')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
