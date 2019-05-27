730522ccfb8c1f6e53acb26e351c6f8305aea74a
106062328 <s106062328@SoftwareStudio.cs.nthu.edu.tw>
2019-05-27 19:20:18 +0800

    login

---
+++ fb/info.php
@@ -0,0 +1,25 @@
+<?php
+require_once('vendor/autoload.php');
+session_start();
+$fb = new Facebook\Facebook([
+  'app_id' => '472713029935315',
+  'app_secret' => '535907ebbb12e0b2297ef875cf803fd1',
+  'default_graph_version' => 'v3.3',
+  ]);
+
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get('/me?fields=id,name', $_SESSION['fb_access_token']);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$user = $response->getGraphUser();
+
+echo 'Name: ' . $user['name'];
+echo '<br>';
+echo 'Id: '.$user['id'];
