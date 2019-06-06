721f5adc6ccd956882b076eb7df6436c44ce214a
s106062129 <icesnow596@gmail.com>
2019-06-06 17:32:40 +0800

    0.1.2

--- chat_room/index.php
+++ chat_room/index.php
@@ -56,8 +56,8 @@
 		$sth->execute();
 		while($friend = $sth->fetch()){
 			var_dump($friend);
-			echo "<tr><td>".$row['chat_room_name']."</td><td>".$row['chat_room_displayname']."</td></tr>";
-			echo "<tr><td> <button>".$row['chat_room_displayname']."</button></td></tr>";//need to set onclick function to go into a chat room
+			echo "<tr><td>".$friend['chat_room_name']."</td><td>".$friend['chat_room_displayname']."</td></tr>";
+			echo "<tr><td> <button>".$friend['chat_room_displayname']."</button></td></tr>";//need to set onclick function to go into a chat room
 		}
 
         $dsn = null;
