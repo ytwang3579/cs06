bdec6fa51d096b3cec6fdb9df72cdbd09a1a7403
106062328 <s106062328@SoftwareStudio.cs.nthu.edu.tw>
2019-06-06 16:38:55 +0800

    lll

--- chat_room/create_private_chatroom.php
+++ chat_room/create_private_chatroom.php
@@ -37,6 +37,7 @@
 			$sth->execute( array($chatroom_name, $_SESSION['name']) );
 			
 			//create chat room data base
+			var_dump($chatroom_name);
 			$sth=$dbh->prepare('create table '.$chatroom_name.'(
 				idx int not null auto-increment,
 				sender varchar(100) not null,
