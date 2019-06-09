c6e13a3ca89a945cca8cdde6a663d24e918e2e83
106062128 <s106062128@SoftwareStudio.cs.nthu.edu.tw>
2019-06-09 23:53:54 +0800

    try to add voting feature

--- node_server/resource/create_vote.js
+++ node_server/resource/create_vote.js
@@ -1,3 +1,3 @@
 function create_vote_area(){
-	return "<div id = 'vote_area' style:position:absolute;left:0px;top:0px;z-index:-1> <form id='form_vote' action=''> <input type = 'text' id = 'theme' autocomplete='off'><br><input type = 'text' id = 'a1' autocomplete='off'><br><input type = 'text' id = 'a2' autocomplete='off'><br><input type = 'text' id = 'a3' autocomplete='off'><br><button>confirm</button><button id = 'cancel'>cancel</button></form></div>"
+	return "<div id = 'vote_area' style:'position:absolute;left:0px;top:0px;z-index:-1'> <form id='form_vote' action=''> <input type = 'text' id = 'theme' autocomplete='off'><br><input type = 'text' id = 'a1' autocomplete='off'><br><input type = 'text' id = 'a2' autocomplete='off'><br><input type = 'text' id = 'a3' autocomplete='off'><br><button>confirm</button><button id = 'cancel'>cancel</button></form></div>"
 }
