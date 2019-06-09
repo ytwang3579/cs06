9dc86e28b4826cb62c5eedc17a3a2d6847993354
106062328 <s106062328@SoftwareStudio.cs.nthu.edu.tw>
2019-06-10 00:19:57 +0800

    d

---
+++ admin/ban.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    
+    if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
+    {
+        func();
+    }
+    function func()
+    {
+        require("../config.php");
+        $dsn='mysql:host='.$CFG['mysql_host'].';dbname='.$CFG['mysql_dbname'].';';
+        try {
+            $dbh = new pdo($dsn, $CFG['mysql_username'],$CFG['mysql_password']);
+        } catch (PDOExpection $e) {
+            echo 'connection failed: '.$e->getmessage();
+        }
+        
+        $sth=$dbh->prepare(' update user_list set silence=1  where id = '.$_POST['id'].';');
+        $sth->execute();
+       
+    }
+?>
