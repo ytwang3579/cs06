856991958871344c75822eae403d1eab8d6965d5
ytwang <ytwang3579@gapp.nthu.edu.tw>
2019-06-12 01:34:30 +0800

    style adjust

--- chat_room/index.php
+++ chat_room/index.php
@@ -25,7 +25,7 @@
   <div class="sidebar">
     <div class="titlebtn">
       <div class="team-menu-info">
-        <a class="pagetitle" href="location.reload();">CS 0.6</a>
+        <a class="pagetitle" onclick="location.reload();">CS 0.6</a>
         <span class="username"><?= $_SESSION['name'] ?></span>
       </div>
       <span onclick="location.href ='logout.php'" 
