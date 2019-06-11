b95c9aa2bc7a63817c7593635c2f1c4933ab8118
szlee118 <szlee118@gmail.com>
2019-06-11 15:46:46 +0800

    chat_room/

--- chat_room/index.php
+++ chat_room/index.php
@@ -30,7 +30,9 @@
         <h1 class="pagetitle">CS 0.6</h1>
         <span class="username"><?= $_SESSION['name'] ?></span>
       </div>
-      <i class="fa fa-sign-out" aria-hidden="true" onclick="location.href ='logout.php'"></i>
+      <i class="fa fa-sign-out" aria-hidden="true" style="z-index:9999;"
+         onclick="location.href ='logout.php'">
+      </i>
     </button>
     <div class="profile">
       <img src="<?= $_SESSION['picture'] ?>" style="width:200px; height:200px;"/>
