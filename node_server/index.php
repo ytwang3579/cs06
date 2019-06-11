6cb225b283dbadc37c628933ebf81e83f63e58e0
106062128 <s106062128@SoftwareStudio.cs.nthu.edu.tw>
2019-06-12 00:45:11 +0800

    add vote to message

--- node_server/index.php
+++ node_server/index.php
@@ -217,12 +217,14 @@
 			
 			var theme = $('<p>').text(data.theme);
 			var vote_table = $('<table>');
-			var tmp_tr = $('<tr>').append($('<th>')).text("Options");
+			var tmp_tr = $('<tr>');
+			tmp_tr.append($('<th>')).text("Options");
 			tmp_tr.append($('<th>')).text("Vote number");
 			vote_table.append(tmp_tr);
 			
 			data.options.forEach(function(item){
-				tmp_tr = $('<tr>').append($('<td>')).text(item);
+				tmp_tr = $('<tr>');
+				tmp_tr.append($('<td>')).text(item);
 				tmp_tr.append($('<td>')).text(data[item]);
 				vote_table.append(tmp_tr);
 			});
