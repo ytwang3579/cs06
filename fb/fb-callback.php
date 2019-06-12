7597266c66d4c306ca33688797e48e57f8dc9bc3
szlee118 <szlee118@gmail.com>
2019-06-12 20:00:36 +0800

    dd

--- fb/fb-callback.php
+++ fb/fb-callback.php
@@ -42,21 +42,16 @@ if (! isset($accessToken)) {
 }
 
 // Logged in
-  //echo '<h3>Access Token</h3>';
-  //var_dump($accessToken->getValue());
 
 // The OAuth 2.0 client handler helps us manage access tokens
 $oAuth2Client = $fb->getOAuth2Client();
 
 // Get the access token metadata from /debug_token
 $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-  //echo '<h3>Metadata</h3>';
-  //var_dump($tokenMetadata);
 
 // Validation (these will throw FacebookSDKException's when they fail)
 $tokenMetadata->validateAppId('472713029935315'); // Replace {app-id} with your app id
-// If you know the user ID this access token belongs to, you can validate it here
-//$tokenMetadata->validateUserId('123');
+
 $tokenMetadata->validateExpiration();
 
 if (! $accessToken->isLongLived()) {
@@ -67,16 +62,65 @@ if (! $accessToken->isLongLived()) {
     echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
     exit;
   }
-
-  //echo '<h3>Long-lived</h3>';
-  var_dump($accessToken->getValue());
 }
 
-$_SESSION['fb_access_token'] = (string) $accessToken;
 
 // User is logged in with a long-lived access token.
-// You can redirect them to a members-only page.
-echo '<br><br><script>location.href="main.php"</script>';
-echo '';
+
+
+// Followings fetch users' information and redirect to main page
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get('/me?fields=id,name,picture.width(100).height(100).as(picture_small)', $_SESSION['fb_access_token']);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+
+$user = $response->getGraphUser();
+
+require_once("../config.php");
+
+try {
+    require_once('../config.php');
+    $dsn='mysql:host=localhost;dbname=cs06';
+    $dbh=new PDO($dsn,$CFG['mysql_username'],$CFG['mysql_password']);
+    
+    $_SESSION['id']=$user['id'];
+    $_SESSION['name']=$user['name'];
+    $_SESSION['picture']='https://graph.facebook.com/'.$user['id'].'/picture?type=large';
+
+    
+    $sql = "CREATE TABLE IF NOT EXISTS ".$user['id']."_friend (
+            friend_name VARCHAR(45) PRIMARY KEY NOT NULL,
+            confirm_friend VARCHAR(45) NOT NULL,
+            friend_id VARCHAR(45) NOT NULL
+    )";
+    
+    $sql2 = "CREATE TABLE IF NOT EXISTS ".$user['id']."_chatlist (
+            chat_room_name VARCHAR(100) PRIMARY KEY NOT NULL,
+            chat_room_displayname VARCHAR(100) NOT NULL,
+            private INT NOT NULL
+    )";
+    
+    $sth = $dbh->prepare($sql);
+    $sth->execute();
+    $sth2 = $dbh->prepare($sql2);
+    $sth2->execute();
+
+
+    $sth=$dbh->prepare('insert into user_list (id,name,picture,silence) values (?,?,?,?) ');
+    $sth->execute(array($user['id'],$user['name'],'https://graph.facebook.com/'.$user['id'].'/picture?type=large',0));
+     
+    // redirect to main page
+    echo '<br><br><<script>location.href="http://cs06.2y.cc/chat_room/index.php"</script>';
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
 
 
