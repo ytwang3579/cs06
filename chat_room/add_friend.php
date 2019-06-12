1c3e01aa1819f51d306fbe8da52e03558513e9d4
szlee118 <szlee118@gmail.com>
2019-06-13 05:55:31 +0800

    d

--- chat_room/add_friend.php
+++ chat_room/add_friend.php
@@ -13,6 +13,7 @@
 <!DOCTYPE html>
 <head>
 	<link rel="stylesheet" type="text/css" href="style4php.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 <?php
