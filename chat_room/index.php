9b25f08f12711a77afcf5d8143871258d4335256
106062128 <s106062128@SoftwareStudio.cs.nthu.edu.tw>
2019-06-13 01:20:51 +0800

    syntax fix

--- chat_room/index.php
+++ chat_room/index.php
@@ -60,7 +60,7 @@
 		    $sth->execute();
 		    while($friend = $sth->fetch()){
           if($friend['private']==0){
-            echo '<li class="chatroomlist-item"><button class="chatroomlist-button" onclick="OpenChatRoom(\''.$friend['chat_room_name'].','.$friend['chat_room_displayname'].'\')"><span>'.$friend['chat_room_displayname'].'</span></button></li>';
+            echo '<li class="chatroomlist-item"><button class="chatroomlist-button" onclick="OpenChatRoom(\''.$friend['chat_room_name'].'\',\''.$friend['chat_room_displayname'].'\')"><span>'.$friend['chat_room_displayname'].'</span></button></li>';
           }
         }
 
@@ -98,7 +98,7 @@
 		    $sth->execute();
 		    while($friend = $sth->fetch()){
           if($friend['private']==1){
-            echo "<li class='chatroomlist-item'><button class='chatroomlist-button' onclick=\"OpenChatRoom('".$friend['chat_room_name'].",".$friend['chat_room_displayname']."')\"><span>".$friend['chat_room_displayname']."</span></button></li>";
+            echo "<li class='chatroomlist-item'><button class='chatroomlist-button' onclick=\"OpenChatRoom('".$friend['chat_room_name']."','".$friend['chat_room_displayname']."')\"><span>".$friend['chat_room_displayname']."</span></button></li>";
           }
         }
 
