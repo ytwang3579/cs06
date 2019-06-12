ac8fec6504c88d0cc944d7b3a812347757c492a2
szlee118 <szlee118@gmail.com>
2019-06-13 06:13:54 +0800

    dd

--- chat_room/create_public_chatroom.php
+++ chat_room/create_public_chatroom.php
@@ -98,7 +98,7 @@
     $sth=$dbh->prepare('select * from '.$_SESSION['id'].'_friend order by friend_name desc;');
 	$sth->execute();
 	echo "<form method=POST>";
-	echo "<input type='text' name=chatroom_name placeholder='type chat room name'>";
+	echo "<input type='text' name=chatroom_name class="form-control" placeholder='type chat room name'>";
 	while($row = $sth->fetch()){//create chat room button to each friend
 		echo "<tr><td>".
 			"<input type='checkbox' name=chatroom_friend[] value=".$row['friend_id'].">".$row['friend_name'].
