41951cc838216342b699c38a0a61c495814f2fe0
szlee118 <szlee118@gmail.com>
2019-06-13 04:06:38 +0800

    d

--- node_server/index.php
+++ node_server/index.php
@@ -24,7 +24,7 @@
 	</div>
 	
 
-	<nav class="navbar navbar-dark bg-light">
+	<nav class="navbar navbar-light bg-light">
      <a class="navbar-brand" href="#">
        <img src="../image.png" width="30" height="30" class="d-inline-block align-top" alt="">
 	   <span id="room_name"></span>
