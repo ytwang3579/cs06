4aae80953d9b2f284b189719945085b6e131cf8e
szlee118 <szlee118@gmail.com>
2019-06-13 11:24:28 +0800

    d

--- chat_room/create_public_chatroom.php
+++ chat_room/create_public_chatroom.php
@@ -106,8 +106,10 @@
 	echo "<input type='text' name='chatroom_name' class='form-control' placeholder='type chat room name'>";
 	while($row = $sth->fetch()){//create chat room button to each friend
 		echo "<tr><td>".
-			"<input type='checkbox' class='custom-control-input' name=chatroom_friend[] value=".$row['friend_id'].">".$row['friend_name'].
-			" </td></tr>";
+			"<div class='custom-control custom-checkbox'>
+			 <input type='checkbox' class='custom-control-input' name=chatroom_friend[] value=".$row['friend_id'].">"
+			."<label class='custom-control-label' for='defaultUnchecked'>".$row['friend_name'].
+			"</label></div></td></tr>";
 	}
 	echo "<input type='submit'  class='btn btn-secondary' value='Create Room'>";
 	echo "</form>";
