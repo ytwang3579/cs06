92b7fb1982d69a113c7dddc8fb6977eb34a5787b
szlee118 <szlee118@gmail.com>
2019-06-13 12:37:38 +0800

    dd

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -77,12 +77,12 @@ if($_SERVER['REQUEST_METHOD']=="POST"){//if post check information
             <h5 class="card-title text-center">Sign In</h5>
             <form class="form-signin" method="POST">
               <div class="form-label-group">
-                <input type="text" id="inputEmail" class="form-control" placeholder="Admin Name" required autofocus>
+                <input type="text" name="user" class="form-control" placeholder="Admin Name" required autofocus>
                 <label for="inputEmail">Admin Name</label>
               </div>
 
               <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <input type="password" name="password" class="form-control" placeholder="Password" required>
                 <label for="inputPassword">Password</label>
               </div>
               <input class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" value="Login">
