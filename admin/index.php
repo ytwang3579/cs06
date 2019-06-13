c749d398b6dd50bce8219fd7fa9bf39318c13284
szlee118 <szlee118@gmail.com>
2019-06-13 12:10:20 +0800

    d

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,8 @@
 <script src="https://code.jquery.com/jquery-1.11.1.js"></script>
 <script src="resource/socket.io.js"></script>
 <script src="https://kit.fontawesome.com/df3e0e4f87.js"></script>
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="main.css">
 </head>
 
